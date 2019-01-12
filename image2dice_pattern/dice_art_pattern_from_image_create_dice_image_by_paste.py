d59673a00295c7a3e3bacacd2e18a563d145f79a
Majeed Askari <majeedaskari96@gmail.com>
2019-01-12 23:23:40 +0330

    now you can control the quality (resolution) of your image

--- image2dice_pattern/dice_art_pattern_from_image_create_dice_image_by_paste.py
+++ image2dice_pattern/dice_art_pattern_from_image_create_dice_image_by_paste.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageOps, ImageDraw
 
-dicew = 100
-
+dicew = 300
+quality = 6 # minimum = 1, maximum = resolution of your dice image / dicesize
 im = Image.open("image.png")
 im = ImageOps.grayscale(im)
 im = ImageOps.equalize(im)
@@ -10,13 +10,14 @@ diceh = im.height / im.width * dicew
 
 dicesize = int(im.width / dicew)
 
-nim = Image.new("L", (im.width, im.height), 'white')
-nimd = ImageDraw.Draw(nim)
+nim = Image.new("L", (im.width * quality, im.height * quality), 'white')
+# nimd = ImageDraw.Draw(nim)
 
 dices = []
 for i in range(1, 7):
     dim = Image.open("dice/" + str(i) + ".jpg")
-    dim = dim.resize((dicesize, dicesize), Image.ANTIALIAS)
+    dim = dim.resize((dicesize * quality, dicesize * quality), Image.ANTIALIAS)
+    dim = ImageOps.equalize(dim)
     dices.append(dim)
 
 for y in range(0, im.height-dicesize, dicesize):
@@ -31,7 +32,7 @@ for y in range(0, im.height-dicesize, dicesize):
         diceNumber = (255-thisSectorColor) * 5 / 255 + 1
         #print (x, y, thisSectorColor, diceNumber)
         # print diceNumber,
-        im.paste(dices[diceNumber - 1], (x, y))
+        nim.paste(dices[diceNumber - 1], (x * quality, y * quality))
     # print
-im.save("diceimage.png")
-im.show()
+nim.save("diceimage.png")
+# nim.show()
